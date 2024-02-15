ca5cca074683933188099f3516749ae8750e714d
Ricardo Machado <45700949+rmgmachado@users.noreply.github.com>
2024-02-15 15:28:29 +1100

    Fixed missing fopen_s for Linux

--- include/rmlib/xplat.h
+++ include/rmlib/xplat.h
@@ -143,17 +143,30 @@
 #endif
 
 #if !defined(XPLAT_OS_WIN)
-   #define _GNU_SOURCE
+   #ifndef _GNU_SOURCE
+      #define _GNU_SOURCE
+   #endif
    #include <string.h>
-   
-   inline int strerror_s(char* buf, size_t buflen, int errnum) noexcept
+   #include <cerrno>
+
+   inline errno_t strerror_s(char* buf, size_t buflen, int errnum) noexcept
    {
       if (!buf || buflen == 0) return EINVAL;
       char* str = strerror_r(errnum, buf, buflen);
       if (str != buf)
       {
-         strlcpy(buf, str, buflen);
+         strncpy(buf, str, buflen);
+         buf[buflen - 1] = '\0';
       }
+      return 0;
+   }
+
+   inline errno_t fopen_s(FILE** pfd, const char* filename, const char* mode) noexcept
+   {
+      if (!pfd || !filename || !mode) return EINVAL;
+      FILE* fd = fopen(filename, mode);
+      if (!fd) return errno;
+      *pfd = fd;
       return 0;
    }
 
